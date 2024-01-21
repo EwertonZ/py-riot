84c0f9e37ada5b191cfea8ca8a37ff7bbb95da18
EwertonZ <ewertonzvd@gmail.com>
2024-01-21 19:45:28 -0300

    wip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py-riot-standard-calls',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         # Dependências necessárias
